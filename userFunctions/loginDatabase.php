9cd5eea7c738b3276a8bf096d2da424f15c91f9a
Calvin Min <32576363+Calvinjmin@users.noreply.github.com>
2021-11-30 20:34:31 -0500

    NO ALERT

--- userFunctions/loginDatabase.php
+++ userFunctions/loginDatabase.php
@@ -10,7 +10,6 @@
 		$(document).ready(function() {
 			$('#changeAthlete').click(function() {
                 var username = $('#usernameSecure').text();
-                alert(username);
 				$.ajax({
             		type: "POST",
 					url: "changeAthlete.php",
